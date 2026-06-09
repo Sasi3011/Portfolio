6c301a14b9b8c672fa02163a6328cf9fd1c0a713
Sasikiran TT <sasikiran.tt3011@gmail.com>
2026-06-09 23:03:38 +0530

    feat(lib): add social links and contact constants

---
+++ src/lib/constants/social.ts
@@ -0,0 +1,5 @@
+export const GITHUB_USERNAME = "Sasi3011";
+export const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`;
+export const LINKEDIN_URL = "https://www.linkedin.com/in/sasikiran-62b77331b";
+export const EMAIL = "sasikiran.tt3011@gmail.com";
+export const PHONE = "+91 8610500527";
